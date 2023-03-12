eba87c85437eecddfc3334270859505cbe56f1af
Kiet Pham <kiet.pham@lapisit.com.au>
2023-03-12 12:02:13 +0700

    Install Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './index.css';
+
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<ChakraProvider>
+			<App />
+		</ChakraProvider>
+	</React.StrictMode>
+);
