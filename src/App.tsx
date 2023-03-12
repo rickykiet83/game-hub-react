009a1f97174d256860cbdc91d45b50c62ee15f3c
Kiet Pham <kiet.pham@lapisit.com.au>
2023-03-12 12:09:40 +0700

    Build Reponsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
-	return <Button colorScheme='blue'>Button</Button>;
+	return (
+		<Grid
+			templateAreas={{
+				base: `"nav" "main"`,
+				lg: `"nav nav" "aside main"`, // 1024px
+			}}
+		>
+			<GridItem area='nav' bg='coral'>
+				Nav
+			</GridItem>
+			<Show above='lg'></Show>
+			<GridItem area='aside' bg='gold'>
+				Asid
+			</GridItem>
+			<GridItem area='main' bg={'dodgerblue'}>
+				Main
+			</GridItem>
+		</Grid>
+	);
 }
 
 export default App;
