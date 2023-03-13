042abbd181c08fae66051a6b7a25dcf4991e1f5c
Kiet Pham <kiet.pham@lapisit.com.au>
2023-03-13 12:19:26 +0700

    Implement search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,20 @@
-import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/logo.webp';
+import { HStack, Image } from '@chakra-ui/react';
+
 import ColorModeSwitch from './ColorModeSwitch';
+import { SearchInput } from './SearchInput';
+import logo from '../assets/logo.webp';
 
-const NavBar = () => {
-  return (
-    <HStack justifyContent='space-between' padding='10px'>
-      <Image src={logo} boxSize='60px' />
-      <ColorModeSwitch />
-    </HStack>
-  )
+interface Props {
+	onSearch: (searchText: string) => void;
 }
+const NavBar = ({ onSearch }: Props) => {
+	return (
+		<HStack padding='10px'>
+			<Image src={logo} boxSize='60px' />
+			<SearchInput onSearch={onSearch} />
+			<ColorModeSwitch />
+		</HStack>
+	);
+};
 
-export default NavBar
+export default NavBar;
