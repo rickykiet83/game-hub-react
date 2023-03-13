d006f2dd9d65a9c1cd5d899ec02c02c723dc545a
Kiet Pham <kiet.pham@lapisit.com.au>
2023-03-13 12:55:33 +0700

    Cleaning up the Game Card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,27 +1,30 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import getCroppedImageUrl from '../services/image-url'
-import CriticScore from './CriticScore'
-import PlatformIconList from './PlatformIconList'
+import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
+
+import CriticScore from './CriticScore';
+import { Game } from '../hooks/useGames';
+import PlatformIconList from './PlatformIconList';
+import React from 'react';
+import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
-  game: Game
+	game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
-  return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
-        <HStack justifyContent='space-between'>
-          <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
-  )
-}
+	return (
+		<Card>
+			<Image src={getCroppedImageUrl(game.background_image)} />
+			<CardBody>
+				<HStack justifyContent='space-between' marginBottom='2'>
+					<PlatformIconList
+						platforms={game.parent_platforms.map((p) => p.platform)}
+					/>
+					<CriticScore score={game.metacritic} />
+				</HStack>
+				<Heading fontSize='2xl'>{game.name}</Heading>
+			</CardBody>
+		</Card>
+	);
+};
 
-export default GameCard
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,41 +1,42 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
-import { GameQuery } from "../App";
-import useGames, { Platform } from "../hooks/useGames";
-import { Genre } from "../hooks/useGenres";
-import GameCard from "./GameCard";
-import GameCardContainer from "./GameCardContainer";
-import GameCardSkeleton from "./GameCardSkeleton";
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import useGames, { Platform } from '../hooks/useGames';
 
-interface Props { 
-  gameQuery: GameQuery
+import GameCard from './GameCard';
+import GameCardContainer from './GameCardContainer';
+import GameCardSkeleton from './GameCardSkeleton';
+import { GameQuery } from '../App';
+import { Genre } from '../hooks/useGenres';
+
+interface Props {
+	gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6];
+	const { data, error, isLoading } = useGames(gameQuery);
+	const skeletons = [1, 2, 3, 4, 5, 6];
 
-  return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
-        spacing={3}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))}
-      </SimpleGrid>
-    </>
-  );
+	return (
+		<>
+			{error && <Text>{error}</Text>}
+			<SimpleGrid
+				columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+				padding='10px'
+				spacing={6}
+			>
+				{isLoading &&
+					skeletons.map((skeleton) => (
+						<GameCardContainer key={skeleton}>
+							<GameCardSkeleton />
+						</GameCardContainer>
+					))}
+				{data.map((game) => (
+					<GameCardContainer key={game.id}>
+						<GameCard game={game} />
+					</GameCardContainer>
+				))}
+			</SimpleGrid>
+		</>
+	);
 };
 
 export default GameGrid;
