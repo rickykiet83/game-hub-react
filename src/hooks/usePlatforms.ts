d012e90dfdf5e893842e3e5444b8d3eea0d534d2
Kiet Pham <kiet.pham@lapisit.com.au>
2023-11-14 21:07:57 +1100

    Fetching platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,18 @@
+import { FetchResponse } from './useData';
+import apiClient from '../services/api-client';
 import platforms from '../data/platforms';
+import { useQuery } from '@tanstack/react-query';
 interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () => useQuery({
+  queryKey: ['platforms'],
+  queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents').then(res => res.data),
+  staleTime: 24 * 60 * 60 * 1000, //24 hours
+  initialData: { count: platforms.length, results: platforms },
+})
 
 export default usePlatforms;
