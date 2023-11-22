067ce85f26190ffdc2b21849d89575e3dfe55a2a
Kiet Pham <kiet.pham@lapisit.com.au>
2023-11-22 13:29:17 +1100

    Building Game Attributes

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,32 @@
+import { Button, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+
+interface Props {
+	children: string;
+}
+
+export const ExpandableText = ({ children }: Props) => {
+	const [expanded, setExpanded] = useState(false);
+
+	const limit = 300;
+	if (!children) return null;
+
+	if (children.length <= limit) return <Text>{children}</Text>;
+
+	const summary = expanded ? children : children.substring(0, limit) + '...';
+
+	return (
+		<Text>
+			{summary}
+			<Button
+				marginLeft={1}
+				fontWeight='bold'
+				colorScheme='yellow'
+				onClick={() => setExpanded(!expanded)}
+				size='xs'
+			>
+				{expanded ? 'Show Less' : 'Read More'}
+			</Button>
+		</Text>
+	);
+};
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,7 @@
-import { Heading, Spinner, Text } from '@chakra-ui/react';
+import { Heading, Spinner } from '@chakra-ui/react';
 
 import { ExpandableText } from '../components/ExpandableText';
+import { GameAttributes } from '../components/GameAttributes';
 import React from 'react';
 import useGame from '../hooks/useGame';
 import { useParams } from 'react-router-dom';
@@ -15,6 +16,7 @@ export const GameDetailPage = () => {
 		<>
 			<Heading>{game.name}</Heading>
 			<ExpandableText>{game.description_raw}</ExpandableText>
+			<GameAttributes game={game} />
 		</>
 	);
 };
