91564b4d5949eaad76eb680283c3e5c23fb625d1
Kiet Pham <kiet.pham@lapisit.com.au>
2023-11-22 12:40:02 +1100

    Building Expandable Text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,6 @@
 import { Heading, Spinner, Text } from '@chakra-ui/react';
 
+import { ExpandableText } from '../components/ExpandableText';
 import React from 'react';
 import useGame from '../hooks/useGame';
 import { useParams } from 'react-router-dom';
@@ -13,7 +14,7 @@ export const GameDetailPage = () => {
 	return (
 		<>
 			<Heading>{game.name}</Heading>
-			<Text>{game.description_raw}</Text>
+			<ExpandableText>{game.description_raw}</ExpandableText>
 		</>
 	);
 };
